03e04986e0df4ed9ce1f6898dc6358bdb619b037
shadman-a <shadman@outlook.com>
2020-11-16 20:34:58 -0500

    Initial commit

---
+++ src/dev/shadmanahmed/Main.java
@@ -0,0 +1,18 @@
+package dev.shadmanahmed;
+
+public class Main {
+
+    public static void main(String[] args) {
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+
+        System.out.println( "Float minimum value =" + myMinFloatValue);
+        System.out.println( "Float maximum value =" + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+
+        System.out.println(myMaxDoubleValue);
+        System.out.println(myMaxDoubleValue);
+    }
+}
